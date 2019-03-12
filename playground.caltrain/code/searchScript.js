a697cd0d51f10271cc4800eecd5ee3d814b70d5b
Peter Jin <peter.jin@sisa.samsung.com>
2019-03-12 15:21:42 -0700

    caltrain station voc and do dont concept

--- playground.caltrain/code/searchScript.js
+++ playground.caltrain/code/searchScript.js
@@ -1,3 +1,10 @@
-module.exports.function = function searchScript (searchInput) {
-  return "output: " + searchInput;
+module.exports.function = function searchScript (dontInput, doInput, searchInput) {
+  
+  if (dontInput != null) {
+    return "escape " + dontInput.toString();
+  }
+  
+  return "output: " + searchInput.toString();
 }
+
+
